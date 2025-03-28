4c38cad66c0e0726cdcc908bac9c534c13cd257e
tonoboas <noboa.l@northeastern.edu>
2025-03-28 04:32:15 +0000

    Final structure for Streamlit multipage

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Flight Dashboard", layout="centered")
 
-st.markdown("## 🛫 Welcome to the U.S. Flight Dashboard System")
+st.markdown("## ✈️ Welcome to the U.S. Flight Dashboard System")
 st.write("Use the menu on the left to:")
 
-st.markdown("- 🔮 **[Predict flight delays](pages/flight_prediction.py)**")
-st.markdown("- 📊 **[Explore our interactive dashboard](pages/tableau_dashboard.py)**")
+st.markdown("- 🔮 **Predict flight delays**")
+st.markdown("- 📊 **Explore our interactive dashboard**")
