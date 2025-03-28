7183bf44f4f45d6d8681ab7fd5fe2235c4d529c8
tonoboas <noboa.l@northeastern.edu>
2025-03-28 14:25:46 +0000

    Fix Streamlit switch_page paths

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import streamlit as st
 
-st.set_page_config(page_title="Flight Dashboard", layout="centered")
+st.set_page_config(page_title="Flight Delay Dashboard", layout="centered")
 
-st.markdown("## ✈️ Welcome to the U.S. Flight Dashboard System")
-st.write("Use the buttons below to navigate:")
+st.markdown("<h1 style='text-align: center;'>✈️ Welcome to the U.S. Flight Delay Dashboard</h1>", unsafe_allow_html=True)
+st.markdown("## ")
+st.markdown("### Use the buttons below to get started:")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown(
-        """
-        <a href="/pages/flight_prediction" target="_self">
-            <button style="font-size:20px;padding:10px 24px;">🔮 Predict Flight Delays</button>
-        </a>
-        """,
-        unsafe_allow_html=True,
-    )
+    if st.button("🔮 Predict Flight Delays"):
+        st.switch_page("flight_prediction.py")
 
 with col2:
-    st.markdown(
-        """
-        <a href="/pages/tableau_dashboard" target="_self">
-            <button style="font-size:20px;padding:10px 24px;">📊 Explore Dashboard</button>
-        </a>
-        """,
-        unsafe_allow_html=True,
-    )
+    if st.button("📊 Explore Dashboard"):
+        st.switch_page("tableau_dashboard.py")
